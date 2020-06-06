a2ee8ee4824b93eccfe41fb628bffbd458dc7647
hddkkll <shs403102@naver.com>
2020-06-06 17:55:28 +0900

    api 로그인창, 계좌 조회

---
+++ __init__.py
@@ -0,0 +1,14 @@
+from kiwoom.kiwoom import *
+import sys
+from PyQt5.QtWidgets import *
+
+class Main():
+    def __init__(self):
+        print("Main() start")
+
+        self.app = QApplication(sys.argv)
+        self.kiwoom = kiwoom()
+        self.app.exec_()
+
+if __name__ == "__main__":
+    Main()
